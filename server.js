5888ccf0ba2aca66544d5e2756a981cb194dbb3a
KevinYan <yanxiangyang94@gmail.com>
2020-02-28 17:23:26 +0800

    server.js

--- server.js
+++ server.js
@@ -10,17 +10,18 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var path = request.url 
-  var query = ''
-  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
-  var pathNoQuery = parsedUrl.pathname
-  var queryObject = parsedUrl.query
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
   var method = request.method
 
   /******** 从这里开始看，上面不要看 ************/
 
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
-
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
 
   if(path === '/'){
     response.statusCode = 200
@@ -28,16 +29,16 @@ var server = http.createServer(function(request, response){
     response.write(`
       <!DOCTYPE html>
       <head>
-        <link rel="stylesheet" href="/style.css"/>
+        <link rel="stylesheet" href="/style"/>
       </head>
 
       <body>
          <h1>你好</h1>
-         <script src="/main.js"></script>
+         <script src="/main"></script>
       </body>
     `)
     response.end()
-  }else if (path === '/style.css'){
+  }else if (path === '/style'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`
@@ -45,25 +46,19 @@ var server = http.createServer(function(request, response){
         h1{color: red}
     `)
     response.end()
-  }else if (path === '/main.js'){
+  }else if (path === '/main'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
     response.write(`
-        alert("这是js执行的哈哈哈")
+        alert("这是js执行的")
     `)
     response.end()    
   }else{
-    response.statusCode = 404 
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('呜呜呜')
     response.end()
   }
-
-
-
-
-
-
-
-
 
   /******** 代码结束，下面不要看 ************/
 })
